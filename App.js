bb57ae13622ef21b545cb74b3c1daa020f2b0273
justJooo <abel.jollando073@student.unud.ac.id>
2023-10-25 04:39:07 +0800

    EDP-13 Move to another repo

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, SafeAreaView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView className='flex-1 justify-center items-center bg-red-200'>
+            <Text>Hello World</Text>
+            <StatusBar style='auto' />
+        </SafeAreaView>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
