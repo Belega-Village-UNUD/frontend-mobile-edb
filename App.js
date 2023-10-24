1a419966871251ec1306c8711a1c451de16320db
justJooo <abel.jollando073@student.unud.ac.id>
2023-10-25 04:47:25 +0800

    EDP-38 Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, SafeAreaView } from 'react-native';
 export default function App() {
     return (
         <SafeAreaView className='flex-1 justify-center items-center bg-red-200'>
-            <Text>Hello World</Text>
+            <Text>Init FE Mobile</Text>
             <StatusBar style='auto' />
         </SafeAreaView>
     );
